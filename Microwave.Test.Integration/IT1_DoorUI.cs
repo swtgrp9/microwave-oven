879d51f3182348b7da916975552af66e29f80805
Plexandie <sebastianmik@hotmail.com>
2019-11-21 13:57:10 +0100

    Added IT project and template for first two steps and gitignore files - seb

---
+++ Microwave.Test.Integration/IT1_DoorUI.cs
@@ -0,0 +1,17 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT1_DoorUI
+    { 
+
+
+        [SetUp]
+        
+    }
+}
