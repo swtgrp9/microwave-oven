337cf43b32db233d2d6f9eb0ba108f353d197b68
bvoelund@hotmail.com <bvoelund@hotmail.com>
2019-11-22 09:23:33 +0100

    No changes

--- Microwave.Test.Integration/IT1_DoorUI.cs
+++ Microwave.Test.Integration/IT1_DoorUI.cs
@@ -1,17 +1,21 @@
-﻿using NSubstitute;
+﻿using MicrowaveOvenClasses.Boundary;
+using NSubstitute;
 using NUnit.Framework;
-using MicrowaveOvenClasses.Boundary;
-using MicrowaveOvenClasses.Controllers;
-using MicrowaveOvenClasses.Interfaces;
+
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
     public class IT1_DoorUI
-    { 
-
+    {
+        Button = new Button
+        
 
         [SetUp]
+        public void SetUp()
+        {
+
+        }
         
     }
 }
