46fb8f1bf289e6efd491214812e8f457eef3af1c
Plexandie <sebastianmik@hotmail.com>
2019-11-28 15:30:41 +0100

    Started tests in step 8 - seb

--- Microwave.Test.Integration/IT8_Output_PwrTubeCcLightTimerDisplayUiDoorButton.cs
+++ Microwave.Test.Integration/IT8_Output_PwrTubeCcLightTimerDisplayUiDoorButton.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 using NSubstitute;
 using NSubstitute.Core.Arguments;
 using NSubstitute.ReceivedExtensions;
@@ -10,6 +12,7 @@ using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     public class IT8_Output_PwrTubeCcLightTimerDisplayUiDoorButton
     {
         private IButton _powerButton;
@@ -33,9 +36,15 @@ namespace Microwave.Test.Integration
 
         private Output _sut;
 
+        private StringWriter stringwriter;
+
         [SetUp]
         public void SetUp()
         {
+
+            stringwriter = new StringWriter();
+            Console.SetOut(stringwriter);
+
             //testing
             _sut = new Output();
 
@@ -52,11 +61,58 @@ namespace Microwave.Test.Integration
 
         }
 
+
+        #region Light-Output
+
         [Test]
-        public void Testtesthest()
+        public void Test_TurnOnLight_ReadyState()
         {
+            _door.Open();
+
+
+            Assert.That(stringwriter.ToString(), Does.StartWith("Light is turned on"));
 
         }
+
+        [Test]
+        public void Test_TurnOffLight_DoorIsOpenState()
+        {
+            _door.Open();
+
+            //Redundant - man kunne skrives contain istedet for StartWith
+            //StringBuilder sb = stringwriter.GetStringBuilder(); //Clear stringwriter så der ikke står "light is turned on" fra når døren åbnes
+            //sb.Remove(0, sb.Length);
+
+            _door.Close();
+
+            Assert.That(stringwriter.ToString(), Does.Contain("Light is turned off"));
+        }
+
+        #endregion
+
+
+        #region Display-Output
+
+        [Test]
+        public void Test_ShowPower_ReadyState()
+        {
+            _powerButton.Press();
+
+            Assert.That(stringwriter.ToString(), Does.StartWith("Display shows: 50 W"));
+        }
+
+        [Test]
+        public void Test_ShowTime_SetPowerState()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+
+            Assert.That(stringwriter.ToString(), Does.Contain("Display shows: 01:00"));
+
+        }
+
+        #endregion
+
     }
 }
 
