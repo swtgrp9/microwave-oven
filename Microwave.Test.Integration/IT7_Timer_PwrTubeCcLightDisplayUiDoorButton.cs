ee6525692a107fabd5b4994deb6cacd6485be02d
Anonym <201710692@post.au.dk>
2019-11-28 15:16:48 +0100

    Tests til timer skulle gerne være done - Umut

--- Microwave.Test.Integration/IT7_Timer_PwrTubeCcLightDisplayUiDoorButton.cs
+++ Microwave.Test.Integration/IT7_Timer_PwrTubeCcLightDisplayUiDoorButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using NSubstitute;
 using NSubstitute.Core.Arguments;
 using NSubstitute.ReceivedExtensions;
@@ -7,6 +8,7 @@ using NUnit.Framework;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace Microwave.Test.Integration
 {
@@ -36,7 +38,6 @@ namespace Microwave.Test.Integration
         {
             //stubs
             _output = Substitute.For<IOutput>();
-            _timer = Substitute.For<ITimer>();
 
 
             //top
@@ -52,6 +53,7 @@ namespace Microwave.Test.Integration
             //includes
             _display = new Display(_output);
             _powerTube = new PowerTube(_output);
+            _timer = new Timer();
 
 
             //Sut
@@ -59,7 +61,49 @@ namespace Microwave.Test.Integration
             _cookController = new CookController(_timer, _display, _powerTube);
             _iut = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
             _cookController.UI = _iut;
+        }
 
+        [Test]
+        public void Start_Cooking_Timer_Start()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+            
+            Thread.Sleep(2000);
+
+            _output.Received(1).OutputLine(Arg.Is("Display shows: 00:58"));
+
+
+        }
+
+        [Test]
+        public void Door_Opened_Timer_Stopped()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+            Thread.Sleep(2000);
+
+            _door.Open();
+            _door.Close();
+
+            _output.Received(1).OutputLine(Arg.Is("Display shows: 01:00"));
+        }
+
+        [Test]
+        public void Timer_Expired()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+            Thread.Sleep(60000);
+
+            _output.Received(1).OutputLine(Arg.Is("Display cleared"));
+            _output.Received(1).OutputLine(Arg.Is("Display shows: 01:00"));
+            _output.Received(1).OutputLine(Arg.Is("PowerTube works with 50 W"));
         }
 
     }
