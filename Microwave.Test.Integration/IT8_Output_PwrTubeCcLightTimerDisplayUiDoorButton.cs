b3e773ad1eb4b1d57294a942f05bcd370d2bb822
Plexandie <sebastianmik@hotmail.com>
2019-11-29 11:09:59 +0100

    Finished tests for final step, step 8 - seb

--- Microwave.Test.Integration/IT8_Output_PwrTubeCcLightTimerDisplayUiDoorButton.cs
+++ Microwave.Test.Integration/IT8_Output_PwrTubeCcLightTimerDisplayUiDoorButton.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using NSubstitute;
 using NSubstitute.Core.Arguments;
 using NSubstitute.ReceivedExtensions;
@@ -9,6 +10,7 @@ using NUnit.Framework;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace Microwave.Test.Integration
 {
@@ -34,6 +36,8 @@ namespace Microwave.Test.Integration
 
         private ILight _light;
 
+        private ITimer _timer;
+
         private Output _sut;
 
         private StringWriter stringwriter;
@@ -49,6 +53,7 @@ namespace Microwave.Test.Integration
             _sut = new Output();
 
             //includes
+            _timer = new Timer();
             _display = new Display(_sut);
             _light = new Light(_sut);
             _door = new Door();
@@ -56,6 +61,7 @@ namespace Microwave.Test.Integration
             _timeButton = new Button();
             _startCancelButton = new Button();
             _powerTube = new PowerTube(_sut);
+            _cookController = new CookController(_timer, _display, _powerTube);
             _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
 
         }
@@ -78,13 +84,36 @@ namespace Microwave.Test.Integration
         {
             _door.Open();
 
-            //Redundant - man kunne skrives contain istedet for StartWith
+            //Redundant - man kunne skrive contain i stedet for StartWith
             //StringBuilder sb = stringwriter.GetStringBuilder(); //Clear stringwriter så der ikke står "light is turned on" fra når døren åbnes
             //sb.Remove(0, sb.Length);
 
             _door.Close();
 
             Assert.That(stringwriter.ToString(), Does.Contain("Light is turned off"));
+        }
+
+        [Test]
+        public void Test_TurnOnLight_StartCooking() //Fejler?
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+
+            Assert.That(stringwriter.ToString(), Does.Contain("Light is turned on"));
+        }
+
+        [Test]
+        public void Test_TurnOffLightCalled_onCookingIsDone()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press(); //Starter cooking
+
+            _userInterface.CookingIsDone(); //Afslutter cooking
+
+            Assert.That(stringwriter.ToString(), Does.Contain("Light is turned off"));
+
         }
 
         #endregion
@@ -109,6 +138,85 @@ namespace Microwave.Test.Integration
             Assert.That(stringwriter.ToString(), Does.Contain("Display shows: 01:00"));
 
         }
+
+        [Test]
+        public void Test_ShowTime_OnTimerTicks()
+        {
+            _cookController.StartCooking(50,60);
+
+            Thread.Sleep(5000);
+
+            Assert.That(stringwriter.ToString(), Does.Contain("Display shows: 00:57"));
+
+        }
+
+        [Test]
+        public void Test_ClearCalled_onCookingIsDone()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();  //Starter cooking
+
+            _userInterface.CookingIsDone(); //Afslutter cooking
+
+            Assert.That(stringwriter.ToString(), Does.Contain("Display cleared"));
+
+        }
+
+        #endregion
+
+        #region PowerTube-Output
+
+        [Test]
+        public void Test_TurnOnPowertube_CookingStart()
+        {
+            //_powerButton.Press();
+            //_timeButton.Press();
+            //_startCancelButton.Press();
+
+            _cookController.StartCooking(50,1);
+
+            Assert.That(stringwriter.ToString(), Does.StartWith("PowerTube works with 50 W"));
+
+        }
+
+        [Test]
+        public void Test_TurnOffPowertube_OnTimerExpired()
+        {
+            _cookController.StartCooking(50,5); //Cooking kører i 5 sekunder
+
+            Thread.Sleep(6000); //venter i 6 sekunder - Cooking er done herefter
+
+            Assert.That(stringwriter.ToString(), Does.Contain("PowerTube turned off")); //Sikrer at powertube slukkes når cooking er done
+
+        }
+
+        [Test]
+        public void Test_Extension_TurnOffPowertube_DoorOpenedDuringCooking()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();  //Starter cooking - sætter systemet i cooking state
+
+            _door.Open();
+
+            Assert.That(stringwriter.ToString(), Does.Contain("PowerTube turned off"));
+        }
+
+        [Test]
+        public void Test_Extension_TurnOffPowertube_StartCancelButtonPressedDuringCooking()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();  //Starter cooking - sætter systemet i cooking state
+
+            Thread.Sleep(2000);
+
+            _startCancelButton.Press();
+
+            Assert.That(stringwriter.ToString(), Does.Contain("PowerTube turned off"));
+        }
+
 
         #endregion
 
